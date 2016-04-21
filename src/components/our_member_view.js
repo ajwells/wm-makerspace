030bd1da8d8a0b039ceee7529a3560d32288d849
ajwells <ajwells@email.wm.edu>
2016-04-20 20:38:00 -0400

    new chart menu

--- src/components/our_member_view.js
+++ src/components/our_member_view.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CountRadarChart from './count_radar_chart';
 
+
 export default class OurMemberView extends React.Component {
 	
 	constructor(props) {
@@ -10,8 +11,8 @@ export default class OurMemberView extends React.Component {
 		}
 	}
 
-	handleUpdate(e) {
-		this.setState({type: e.target.value});
+	handleUpdate(item) {
+		this.setState({type: item});
 	}
 
 	getSelected(id, data) {
@@ -27,28 +28,26 @@ export default class OurMemberView extends React.Component {
 	render() {
 		
 		var types = ['interest'];
-		var type = this.state.type;
-		var typeDrop = <select value={this.state.id} className="ui compact search dropdown" id="typeDrop" onChange={this.handleUpdate.bind(this)}>
-			<option value='interests'>interested in</option>
-			<option value='skills'>skilled in</option>
-			<option value='certifications'>certified in</option>
-		</select>
+		var view = this.state.type;
+		var isActive = v => v === view ? 'active' : '';
 
-		return <div className="ui grid"> 
-			<div className="four wide column">
-				<div className="ui yellow segment" >
-					<div className="ui form" style={{padding: '30px'}}>	
-						<div className="field">
-						<label>Our Members are</label>
-							{typeDrop}
-						</div>
+		return <div className="ui yellow segment">
+			<div className="ui grid"> 
+				<div className="four wide column">
+					<div className="ui vertical pointing  menu">
+						<a onClick={this.handleUpdate.bind(this, 'interests')} className={`item ${isActive('interests')}`}>
+							Interests	
+						</a>
+						<a onClick={this.handleUpdate.bind(this, 'skills')} className={`item ${isActive('skills')}`}>
+							Skills	
+						</a>
+						<a onClick={this.handleUpdate.bind(this, 'certifications')} className={`item ${isActive('certifications')}`}>
+							Certifications	
+						</a>
 					</div>
 				</div>
-			</div>
-			<div className="two wide column"></div>
-			<div className="eight wide column">
-				<div className="ui yellow segment">
-					<CountRadarChart type={type}/>
+				<div className="twelve wide column">
+					<CountRadarChart type={this.state.type} />
 				</div>
 			</div>
 		</div>
